c2692482b4678ad22ab1a1eeee882b6d34cd21c2
khisakuni <khisakuni@gmail.com>
2018-12-02 21:12:23 -0800

    Day 1 part 2

--- 2018/day_1/main.go
+++ 2018/day_1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -32,9 +33,35 @@ func main() {
 		return
 	}
 
+	fmt.Println("Part 1")
 	fmt.Printf("answer is: %d\n", reduce(numbers, 0))
 
+	fmt.Println("Part 2")
+	fmt.Printf("answer is: %d\n", repeat(numbers))
+
 	os.Exit(0)
+}
+
+func repeat(numbers []int) int {
+	var r int
+	var err error
+	cache := map[int]bool{r: true}
+	r, err = findRepeat(numbers, r, cache)
+	for err != nil {
+		r, err = findRepeat(numbers, r, cache)
+	}
+	return r
+}
+
+func findRepeat(numbers []int, agg int, cache map[int]bool) (int, error) {
+	for _, n := range numbers {
+		agg += n
+		if v := cache[agg]; v {
+			return agg, nil
+		}
+		cache[agg] = true
+	}
+	return agg, errors.New("no repeat found")
 }
 
 func reduce(ar []int, agg int) int {
--- 2018/day_1/main_test.go
+++ 2018/day_1/main_test.go
@@ -4,7 +4,49 @@ import (
 	"testing"
 )
 
+func TestFindRepeat(t *testing.T) {
+	type testCase struct {
+		in  []int
+		out int
+	}
+	tests := []testCase{
+		testCase{
+			in:  []int{1, -1},
+			out: 0,
+		},
+		testCase{
+			in:  []int{1, -2, 3, 1, 1, -2},
+			out: 2,
+		},
+		testCase{
+			in:  []int{3, 3, 4, -2, -4},
+			out: 10,
+		},
+		testCase{
+			in:  []int{-6, 3, 8, 5, -6},
+			out: 5,
+		},
+		testCase{
+			in:  []int{7, 7, -2, -7, -4},
+			out: 14,
+		},
+	}
+	for _, test := range tests {
+		if res := repeat(test.in); res != test.out {
+			t.Errorf("Expected %d, got %d", test.out, res)
+		}
+	}
+}
+
 func TestReduce(t *testing.T) {
+	type in struct {
+		numbers []int
+		agg     int
+	}
+	type testCase struct {
+		in  in
+		out int
+	}
 	tests := []testCase{
 		testCase{
 			in: in{
@@ -41,14 +83,4 @@ func TestReduce(t *testing.T) {
 			t.Errorf("Expected %d, got %d", test.out, res)
 		}
 	}
-}
-
-type testCase struct {
-	in  in
-	out int
-}
-
-type in struct {
-	numbers []int
-	agg     int
 }
