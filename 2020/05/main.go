2c8c1d3a3bd6b52568fdd2a83e63e43a8f76f0b0
khisakuni <khisakuni@gmail.com>
2020-12-12 22:52:55 -0800

    day 5 part 2

--- 2020/05/main.go
+++ 2020/05/main.go
@@ -12,20 +12,43 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	rowCount := 128
+	colCount := 8
+	m := make([][]int, rowCount)
+	for i := range m {
+		m[i] = make([]int, colCount)
+	}
+
 	strs := strings.Split(string(data), "\n")
-	var max float64
+	//var max float64
 	for _, str := range strs {
 		if str == "" {
 			continue
 		}
 		row, col, id := parse(str)
-		fmt.Printf("%s\n", str)
-		fmt.Printf("  row %f, column %f, seat ID %f.\n", row, col, id)
-		if id > max {
-			max = id
+		m[int(row)][int(col)] = int(id)
+	}
+	for j, r := range m {
+		hasVal := false
+		hasEmpty := false
+		found := false
+		for i, c := range r {
+			if c > 0 && !hasVal {
+				hasVal = true
+				continue
+			}
+			if hasVal && c == 0 && !hasEmpty {
+				hasEmpty = true
+				continue
+			}
+			if hasEmpty && hasVal && c > 0 && !found {
+				found = true
+				fmt.Printf("found it: %v, %v, %v\n", j, i - 1, j * 8 + (i - 1))
+				continue
+			}
 		}
 	}
-	fmt.Printf("max: %v\n", max)
+	//fmt.Printf("max: %v\n", m)
 }
 
 func parse(str string) (row, col, id float64) {
