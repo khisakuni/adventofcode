22bdb40c458284a4fb00cda044c114ce7de70dba
khisakuni <khisakuni@gmail.com>
2021-02-01 09:19:36 -0800

    day 17 part 1

--- 2020/17/main.go
+++ 2020/17/main.go
@@ -18,6 +18,14 @@ func (c cell) print() {
 	}
 }
 
+func (c cell) printColor() {
+	if c.active {
+		fmt.Print("\x1b[6;30;42m#\x1b[0m")
+	} else {
+		fmt.Print("\x1b[6;30;42m.\x1b[0m")
+	}
+}
+
 type coords struct {
 	x int
 	y int
@@ -43,41 +51,127 @@ func (w world) printState() {
 	fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 }
 
-func (w world) activeCount() int {
-	return 0
+func (w world) printStateForCell(coord coords) {
+	fmt.Println("vvvvvvvvvvvvvvvvvvvvvvvvvvvvv")
+	for z, f := range w {
+		fmt.Printf("z=%d\n", z)
+		for y, slice := range f {
+			for x, c := range slice {
+				if z==coord.z && y == coord.y && x ==coord.x {
+					c.printColor()
+					continue
+				}
+				c.print()
+			}
+			fmt.Println()
+		}
+		fmt.Println()
+	}
+	fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 }
 
+
 func (w world) cycle() world {
-	next := w.grow()
-	for z := 1; z < w.depth(); z++ {
-		for y := 1; y < w.height(); y++ {
-			for x := 1; x < w.width(); x++ {
-				//next[z][y][x]
-				//c.print()
+	current := w.grow()
+	next := current.copy()
+	for z := 0; z < current.depth(); z++ {
+		for y := 0; y < current.height(); y++ {
+			for x := 0; x < current.width(); x++ {
+				active := current.isActive(coords{z:z, y:y, x:x})
+				next[z][y][x] = cell{active: active}
 			}
-			//fmt.Println()
 		}
-		//fmt.Println()
 	}
 	return next
 }
 
-/*
-         y      z
-         |     /
-         |    /
-         |   /
-         |  /
-         | /
-_________|/__________ x
-				 /
-				/|
-       / |
-      /  |
-     /   |
+func (w world) isActive(coord coords) bool {
+	neighbors := w.neighbors(coord)
+	count := activeCount(neighbors)
+	c := w[coord.z][coord.y][coord.x]
+	var active bool
+	if c.active {
+		active = count == 2 || count == 3
+	} else {
+		active = count == 3
+	}
+	return active
+}
 
-*/
+func (w world) copy() world {
+	dupe := make(world, w.depth())
+	for z, f := range w {
+		dupe[z] = make(frame, w.height())
+		for y, r := range f {
+			dupe[z][y] = make(row, w.width())
+			for x, c := range r {
+				dupe[z][y][x] = cell{active: c.active}
+			}
+		}
+	}
+	return dupe
+}
 
+func (w world) activeCount() int {
+	var count int
+	for _, f := range w {
+		for _, r := range f {
+			for _, c := range r {
+				if c.active {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+func activeCount(cells []cell) int {
+	var count int
+	for _, c := range cells {
+		if c.active {
+			count++
+		}
+	}
+	return count
+}
+
+func (w world) neighbors(coord coords) []cell {
+	zs := []int{coord.z}
+	if coord.z > 0 {
+		zs = append(zs, coord.z-1)
+	}
+	if coord.z < w.depth()-1 {
+		zs = append(zs, coord.z+1)
+	}
+	ys := []int{coord.y}
+	if coord.y > 0 {
+		ys = append(ys, coord.y-1)
+	}
+	if coord.y < w.height()-1 {
+		ys = append(ys, coord.y+1)
+	}
+	xs := []int{coord.x}
+	if coord.x > 0 {
+		xs = append(xs, coord.x-1)
+	}
+	if coord.x < w.width()-1 {
+		xs = append(xs, coord.x+1)
+	}
+
+	var neighbors []cell
+	for _, z := range zs {
+		for _, y := range ys {
+			for _, x := range xs {
+				if z == coord.z && y == coord.y && x == coord.x {
+					continue
+				}
+				neighbors = append(neighbors, w[z][y][x])
+			}
+		}
+	}
+	return neighbors
+}
 
 func (w world) grow() world {
 	next := w
@@ -151,11 +245,7 @@ func main() {
 		f := w[0]
 		w[0] = append(f, slice)
 	}
-	w.printState()
-
-	fmt.Println(">>>>>>>>>>>>>>>>>>")
-	w = w.cycle().cycle().cycle()
-
-	w.printState()
+	w = w.cycle().cycle().cycle().cycle().cycle().cycle()
+	fmt.Printf("active: %d\n", w.activeCount())
 }
 
