13c96616c4e929e3e0657c1735568ceb6419da0f
khisakuni <khisakuni@gmail.com>
2022-12-04 00:02:15 -0800

    2022 day 4 part 1

---
+++ 2022/04/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	strs := strings.Split(string(input), "\n")
+	var count int
+	for _, str := range strs {
+		if str == "" {
+			continue
+		}
+
+		elves := strings.Split(str, ",")
+		elf1 := strings.Split(elves[0], "-")
+		elf1Min, _ := strconv.Atoi(elf1[0])
+		elf1Max, _ := strconv.Atoi(elf1[1])
+
+		elf2 := strings.Split(elves[1], "-")
+		elf2Min, _ := strconv.Atoi(elf2[0])
+		elf2Max, _ := strconv.Atoi(elf2[1])
+
+		elf1InElf2 := elf1Min >= elf2Min && elf1Max <= elf2Max
+		elf2InElf1 := elf2Min >= elf1Min && elf2Max <= elf1Max
+		if elf1InElf2 || elf2InElf1 {
+			count++
+		}
+	}
+
+	fmt.Printf("Count: %v\n", count)
+}
