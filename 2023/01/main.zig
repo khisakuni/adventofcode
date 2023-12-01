452e56b30d9c61a8ff8359d59ed6be3f08750eed
khisakuni <khisakuni@gmail.com>
2023-12-01 12:10:34 -0800

    Do part 2 in zig

--- 2023/01/main.zig
+++ 2023/01/main.zig
@@ -1,56 +1,98 @@
 const std = @import("std");
 
 pub fn main() !void {
-  // Open input file.
-  var file = try std.fs.cwd().openFile("input.txt", .{ .mode = .read_only });
-  defer file.close();
+    // Open input file.
+    var file = try std.fs.cwd().openFile("input.txt", .{ .mode = .read_only });
+    defer file.close();
 
-  // Get file info.
-  var file_stat = try file.stat();
+    // Get file info.
+    var file_stat = try file.stat();
 
-  // Get file size.
-  const file_size = file_stat.size;
+    // Get file size.
+    const file_size = file_stat.size;
 
-  // Initialize allocator.
-  const allocator = std.heap.page_allocator;
+    // Initialize allocator.
+    const allocator = std.heap.page_allocator;
 
-  // Read file contents.
-  const buffer = try file.readToEndAlloc(allocator, file_size);
-  defer allocator.free(buffer);
+    // Read file contents.
+    const buffer = try file.readToEndAlloc(allocator, file_size);
+    defer allocator.free(buffer);
 
-  var map = std.AutoHashMap(u8, u8).init(allocator);
-  try map.put('0', 0);
-  try map.put('1', 1);
-  try map.put('2', 2);
-  try map.put('3', 3);
-  try map.put('4', 4);
-  try map.put('5', 5);
-  try map.put('6', 6);
-  try map.put('7', 7);
-  try map.put('8', 8);
-  try map.put('9', 9);
+    var map = std.StringHashMap(u8).init(allocator);
+    try map.put("1", 1);
+    try map.put("one", 1);
+    try map.put("2", 2);
+    try map.put("two", 2);
+    try map.put("3", 3);
+    try map.put("three", 3);
+    try map.put("4", 4);
+    try map.put("four", 4);
+    try map.put("5", 5);
+    try map.put("five", 5);
+    try map.put("6", 6);
+    try map.put("six", 6);
+    try map.put("7", 7);
+    try map.put("seven", 7);
+    try map.put("8", 8);
+    try map.put("eight", 8);
+    try map.put("9", 9);
+    try map.put("nine", 9);
+    defer map.deinit();
 
+    const candidates = [_][]const u8{
+        "1",
+        "one",
+        "2",
+        "two",
+        "3",
+        "three",
+        "4",
+        "four",
+        "5",
+        "five",
+        "6",
+        "six",
+        "7",
+        "seven",
+        "8",
+        "eight",
+        "9",
+        "nine",
+    };
 
-  var total: u32 = 0;
-  var is_new_line = true;
-  var first: u8 = 0;
-  var last: u8 = 0;
-  for (buffer) |character| {
-    const num = map.get(character);
-    if (num) |value| {
-      if (is_new_line) {
-        first = value;
-        is_new_line = false;
-      }
-      last = value;
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+    var lines = std.ArrayList(std.ArrayList(u8)).init(allocator);
+    defer lines.deinit();
+
+    for (buffer) |character| {
+        if (character == '\n') {
+            try lines.append(line);
+            line = std.ArrayList(u8).init(allocator);
+            continue;
+        }
+
+        try line.append(character);
     }
 
-    if (character == '\n') {
-      is_new_line = true;
-      const calibration_value = (first * 10) + last;
-      total += calibration_value;
-    }
-  }
+    var total: u32 = 0;
+    for (lines.items) |l| {
+        std.debug.print("line: {s}\n", .{l.items});
+        var list = std.ArrayList([]const u8).init(allocator);
+        defer list.deinit();
+        for (0..l.items.len) |i| {
+            for (candidates) |candidate| {
+                if (l.items.len - i >= candidate.len and std.mem.eql(u8, candidate, l.items[i .. i + candidate.len])) {
+                    std.debug.print("c: {s}: {d}\n", .{ candidate, l.items.len });
+                    try list.append(candidate);
+                }
+            }
+        }
 
-  std.debug.print("Total {d}\n", .{total});
+        var first = map.get(list.items[0]).?;
+        var last = map.get(list.items[list.items.len - 1]).?;
+        total += first * 10 + last;
+    }
+
+    std.debug.print("Total {d}\n", .{total});
 }
