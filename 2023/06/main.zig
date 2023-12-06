3750940e391458fe9640335c4081b74a3f31911f
khisakuni <khisakuni@gmail.com>
2023-12-06 11:24:09 -0800

    2023 day 06 part 2

--- 2023/06/main.zig
+++ 2023/06/main.zig
@@ -13,48 +13,46 @@ pub fn main() !void {
     const buffer = try file.readToEndAlloc(allocator, file_size);
     defer allocator.free(buffer);
 
-    var times = std.ArrayList(i32).init(allocator);
+    var times = std.ArrayList(u8).init(allocator);
     defer times.deinit();
 
-    var dists = std.ArrayList(i32).init(allocator);
+    var dists = std.ArrayList(u8).init(allocator);
     defer dists.deinit();
 
     var lines_iter = std.mem.splitAny(u8, buffer, "\n");
     var i: u8 = 0;
     while (lines_iter.next()) |line| {
-        var line_iter = std.mem.splitAny(u8, line, " ");
-        while (line_iter.next()) |word| {
-            var num = std.fmt.parseInt(i32, word, 10) catch {
-                continue;
-            };
-            if (i == 0) {
-                try times.append(num);
-            }
-            if (i == 1) {
-                try dists.append(num);
+        for (line) |char| {
+            if (std.ascii.isDigit(char)) {
+                if (i == 0) {
+                    try times.append(char);
+                }
+                if (i == 1) {
+                    try dists.append(char);
+                }
             }
         }
         i += 1;
     }
 
-    var total: f32 = 1;
-    for (0.., times.items) |index, time| {
-        const a: f32 = -1;
-        const b: f32 = @floatFromInt(time);
-        const c: f32 = @floatFromInt(-1 * dists.items[index]);
+    const time = try std.fmt.parseInt(i64, times.items, 10);
+    const dist = try std.fmt.parseInt(i64, dists.items, 10);
 
-        const res = try quadratic(a, b, c);
-        const min = @ceil(res[0]);
-        const max = @floor(res[1]);
-        total *= max - min + 1;
-    }
-    std.debug.print("answer: {d}\n", .{total});
+    const a: f64 = -1;
+    const b: f64 = @floatFromInt(time);
+    const c: f64 = @floatFromInt(-1 * dist);
+
+    const res = try quadratic(a, b, c);
+    const min = @ceil(res[0]);
+    const max = @floor(res[1]);
+
+    std.debug.print("answer: {d}\n", .{max - min + 1});
 }
 
-fn quadratic(a: f32, b: f32, c: f32) ![2]f32 {
-    const d: f32 = b * b - 4 * a * c;
-    const d_sqrt: f32 = @sqrt(d);
+fn quadratic(a: f64, b: f64, c: f64) ![2]f64 {
+    const d: f64 = b * b - 4 * a * c;
+    const d_sqrt: f64 = @sqrt(d);
     const root1 = (-b + d_sqrt) / (2 * a);
     const root2 = (-b - d_sqrt) / (2 * a);
-    return [2]f32{ root1, root2 };
+    return [2]f64{ root1, root2 };
 }
