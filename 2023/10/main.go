687ce1c3b25cb8fe0e7aea41e93ff9b026ef7c0f
khisakuni <khisakuni@gmail.com>
2023-12-11 00:43:39 -0800

    2023 day 10 part 2

--- 2023/10/main.go
+++ 2023/10/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	lines := strings.Split(string(data), "\n")
 
-	var queue [][]int
+	var found bool
 
 	// Look for S
 	var i, j int
@@ -23,169 +23,303 @@ func main() {
 		line := lines[i]
 		for j < len(line) {
 			if lines[i][j] == 'S' {
-				queue = append(queue, []int{0, i, j})
-				[]byte(line)[j] = '0'
+				found = true
+				break
+				// queue = append(queue, []int{0, i, j})
+				// []byte(line)[j] = '0'
 			}
 
 			j++
 		}
 
+		if found {
+			break
+		}
+
 		i++
 	}
 
-	max := 0
+	startI := i
+	startJ := j
 
-	fmt.Printf("start: (%v, %v)\n", queue[0][0], queue[0][1])
+	isStart := func(nextI, nextJ int) bool {
+		return nextI == startI && nextJ == startJ
+	}
 
-	for len(queue) > 0 {
-		var current []int
-		current, queue = queue[0], queue[1:]
+	var direction string
 
-		count, i, j := current[0], current[1], current[2]
-
-		if count > max {
-			max = count
-		}
-
+	for !visited(lines[i][j]) {
+		// fmt.Printf("CHAR: %v\n", string(lines[i][j]))
+		// fmt.Println(strings.Join(lines, "\n"))
 		char := lines[i][j]
-
-		line := []byte(lines[i])
-		line[j] = 'x'
-		lines[i] = string(line)
-
 		switch char {
-		case 'x':
-			continue
 		case '|':
 			// Check up
-			if i > 0 {
+			if direction == "up" {
 				next := lines[i-1][j]
-				if next == '|' || next == '7' || next == 'F' {
-					queue = append(queue, []int{count + 1, i - 1, j})
+				if next == '|' || next == '7' || next == 'F' || visited(next) {
+					if isStart(i-1, j) {
+						l := []byte(lines[i-1])
+						l[j] = '^'
+						lines[i-1] = string(l)
+					}
+					direction = "up"
+					l := []byte(lines[i])
+					l[j] = '^'
+					lines[i] = string(l)
+					i = i - 1
+					continue
 				}
 			}
 
 			// Check down
-			if i < len(lines)-1 {
+			if direction == "down" {
 				next := lines[i+1][j]
-				if next == '|' || next == 'L' || next == 'J' {
-					queue = append(queue, []int{count + 1, i + 1, j})
+				if next == '|' || next == 'L' || next == 'J' || visited(next) {
+					if isStart(i+1, j) {
+						l := []byte(lines[i+1])
+						l[j] = 'v'
+						lines[i+1] = string(l)
+					}
+					direction = "down"
+					l := []byte(lines[i])
+					l[j] = 'v'
+					lines[i] = string(l)
+					i = i + 1
+					continue
 				}
 			}
 		case '-':
 			// Check left
-			if j > 0 {
+			if direction == "left" {
 				next := lines[i][j-1]
-				if next == 'L' || next == 'F' || next == '-' {
-					queue = append(queue, []int{count + 1, i, j - 1})
+				if next == 'L' || next == 'F' || next == '-' || visited(next) {
+					direction = "left"
+					l := []byte(lines[i])
+					l[j] = '<'
+					lines[i] = string(l)
+					j = j - 1
+					continue
 				}
 			}
 
 			// Check right
-			if j < len(lines[0])-1 {
+			if direction == "right" {
 				next := lines[i][j+1]
-				if next == '7' || next == 'J' || next == '-' {
-					queue = append(queue, []int{count + 1, i, j + 1})
+				if next == '7' || next == 'J' || next == '-' || visited(next) {
+					direction = "right"
+					l := []byte(lines[i])
+					l[j] = '>'
+					lines[i] = string(l)
+					j = j + 1
+					continue
 				}
 			}
 		case '7':
 			// Check down
-			if i < len(lines)-1 {
+			if direction == "right" {
 				next := lines[i+1][j]
-				if next == '|' || next == 'L' || next == 'J' {
-					queue = append(queue, []int{count + 1, i + 1, j})
+				if next == '|' || next == 'L' || next == 'J' || visited(next) {
+					// if isStart(i+1, j) {
+					// 	l := []byte(lines[i+1])
+					// 	l[j] = 'v'
+					// 	lines[i+1] = string(l)
+					// }
+					direction = "down"
+					l := []byte(lines[i])
+					l[j] = 'v'
+					lines[i] = string(l)
+					i = i + 1
+					continue
 				}
 			}
 
 			// Check left
-			if j > 0 {
+			if direction == "up" {
 				next := lines[i][j-1]
-				if next == '-' || next == 'L' || next == 'F' {
-					queue = append(queue, []int{count + 1, i, j - 1})
+				if next == '-' || next == 'L' || next == 'F' || visited(next) {
+					direction = "left"
+					l := []byte(lines[i])
+					l[j] = '^'
+					lines[i] = string(l)
+					j = j - 1
+					continue
 				}
 			}
 		case 'F':
 			// Check down
-			if i < len(lines)-1 {
+			if direction == "left" {
 				next := lines[i+1][j]
-				if next == '|' || next == 'L' || next == 'J' {
-					queue = append(queue, []int{count + 1, i + 1, j})
+				if next == '|' || next == 'L' || next == 'J' || visited(next) {
+					direction = "down"
+					l := []byte(lines[i])
+					l[j] = 'v'
+					lines[i] = string(l)
+					i = i + 1
+					continue
 				}
 			}
 
 			// Check right
-			if i < len(lines[0])-1 {
+			if direction == "up" {
 				next := lines[i][j+1]
-				if next == '-' || next == 'J' || next == '7' {
-					queue = append(queue, []int{count + 1, i, j + 1})
+				if next == '-' || next == 'J' || next == '7' || visited(next) {
+					direction = "right"
+					l := []byte(lines[i])
+					l[j] = '^'
+					lines[i] = string(l)
+					j = j + 1
+					continue
 				}
 			}
 		case 'J':
 			// Check up
-			if i > 0 {
+			if direction == "right" {
 				next := lines[i-1][j]
-				if next == '|' || next == '7' || next == 'F' {
-					queue = append(queue, []int{count + 1, i - 1, j})
+				if next == '|' || next == '7' || next == 'F' || visited(next) {
+					direction = "up"
+					l := []byte(lines[i])
+					l[j] = '^'
+					lines[i] = string(l)
+					i = i - 1
+					continue
 				}
 			}
 
 			// Check left
-			if j > 0 {
+			if direction == "down" {
 				next := lines[i][j-1]
-				if next == '-' || next == 'L' || next == 'F' {
-					queue = append(queue, []int{count + 1, i, j - 1})
+				if next == '-' || next == 'L' || next == 'F' || visited(next) {
+					direction = "left"
+					l := []byte(lines[i])
+					l[j] = 'v'
+					lines[i] = string(l)
+					j = j - 1
+					continue
 				}
 			}
 		case 'L':
 			// Check up
-			if i > 0 {
+
+			if direction == "left" {
 				next := lines[i-1][j]
-				if next == '|' || next == '7' || next == 'F' {
-					queue = append(queue, []int{count + 1, i - 1, j})
+				if next == '|' || next == '7' || next == 'F' || visited(next) {
+					direction = "up"
+					l := []byte(lines[i])
+					l[j] = '^'
+					lines[i] = string(l)
+					i = i - 1
+					continue
 				}
 			}
 
 			// Check right
-			if j < len(lines[0])-1 {
+			if direction == "down" {
 				next := lines[i][j+1]
-				if next == '-' || next == '7' || next == 'J' {
-					queue = append(queue, []int{count + 1, i, j + 1})
+				if next == '-' || next == '7' || next == 'J' || visited(next) {
+					direction = "right"
+					l := []byte(lines[i])
+					l[j] = 'v'
+					lines[i] = string(l)
+					j = j + 1
+					continue
 				}
 			}
 		case 'S':
 			// Check up
 			if i > 0 {
 				next := lines[i-1][j]
-				if next == '|' || next == '7' || next == 'F' {
-					queue = append(queue, []int{count + 1, i - 1, j})
+				if next == '|' || next == '7' || next == 'F' || visited(next) {
+					direction = "up"
+					l := []byte(lines[i])
+					l[j] = '^'
+					lines[i] = string(l)
+					i = i - 1
+					continue
 				}
 			}
 
 			// Check right
 			if j < len(lines[0])-1 {
 				next := lines[i][j+1]
-				if next == '-' || next == '7' || next == 'J' {
-					queue = append(queue, []int{count + 1, i, j + 1})
+				if next == '-' || next == '7' || next == 'J' || visited(next) {
+					direction = "right"
+					l := []byte(lines[i])
+					l[j] = '>'
+					lines[i] = string(l)
+					j = j + 1
+					continue
 				}
 			}
 
 			// Check down
 			if i < len(lines)-1 {
 				next := lines[i+1][j]
-				if next == '|' || next == 'L' || next == 'J' {
-					queue = append(queue, []int{count + 1, i + 1, j})
+				if next == '|' || next == 'L' || next == 'J' || visited(next) {
+					direction = "down"
+					l := []byte(lines[i])
+					l[j] = 'v'
+					lines[i] = string(l)
+					i = i + 1
+					continue
 				}
 			}
 
 			// Check left
 			if j > 0 {
 				next := lines[i][j-1]
-				if next == '-' || next == 'L' || next == 'F' {
-					queue = append(queue, []int{count + 1, i, j - 1})
+				if next == '-' || next == 'L' || next == 'F' || visited(next) {
+					direction = "left"
+					l := []byte(lines[i])
+					l[j] = '<'
+					lines[i] = string(l)
+					j = j - 1
+					continue
 				}
 			}
 		}
 	}
 
-	fmt.Printf("max: %v\n", max)
+	fmt.Println(strings.Join(lines, "\n"))
+
+	var total int
+	for i, line := range lines {
+		var count int
+		var prev byte
+		for j, c := range line {
+			switch c {
+			case '^':
+				// fmt.Printf("(%d, %d) %v, prev: %v\n", i, j, string(c), string(prev))
+				if prev == 'v' || prev == 0 {
+					count++
+				}
+				prev = '^'
+			case 'v':
+				// fmt.Printf("(%d, %d) %v, prev: %v\n", i, j, string(c), string(prev))
+				if prev == '^' || prev == 0 {
+					count++
+				}
+				prev = 'v'
+			case '<', '>':
+			default:
+				l := []byte(lines[i])
+				if count%2 != 0 {
+					total++
+					l[j] = 'I'
+				} else {
+					l[j] = 'O'
+				}
+				lines[i] = string(l)
+			}
+		}
+	}
+
+	fmt.Println(strings.Join(lines, "\n"))
+
+	fmt.Printf("total: %v\n", total)
+
+}
+
+func visited(char byte) bool {
+	return char == '^' || char == '>' || char == 'v' || char == '<'
 }
