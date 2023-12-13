d6abb31b779ca0b1eb35e6ba5a4eee2daba6cf35
khisakuni <khisakuni@gmail.com>
2023-12-12 21:06:34 -0800

    2023 day 12 part 2

--- 2023/12/main.go
+++ 2023/12/main.go
@@ -9,7 +9,7 @@ import (
 
 /*
 
-Credit to https://github.com/rumkugel13/AdventOfCode2023/blob/main/day12.go
+Credit to https://github.com/macos-fuse-t/aoc/blob/main/2023/12/main.go
 
 */
 
@@ -32,65 +32,138 @@ func main() {
 		}
 
 		springs := parts[0]
-		// res := dp(0, 0, record, groups, cache)
-		total += countVariations([]byte(springs), groups, 0)
+
+		var unfoldedSprings []string
+		var unfoldedGroups []int
+		for i := 0; i < 5; i++ {
+			unfoldedSprings = append(unfoldedSprings, springs)
+			unfoldedGroups = append(unfoldedGroups, groups...)
+		}
+
+		groups = unfoldedGroups
+
+		springs = strings.Join(unfoldedSprings, "?")
+
+		// fmt.Printf("springs: %v\n", springs)
+		// fmt.Printf("groups: %v\n", unfoldedGroups)
+
+		cache := map[string]int{}
+		// total += countVariations([]byte(springs), groups, 0, cache)
+		// fmt.Printf("total: %v\n", total)
+		total += countArrangements(springs, groups, cache)
 
 	}
 
 	fmt.Printf("res: %v\n", total)
 }
 
-func countVariations(springs []byte, groups []int, start int) int {
-	for i := start; i < len(springs); i++ {
-		if springs[i] == '?' {
-			// Encountered '?'; generate two generations:
-			// One with an opeartion spring and the other with a broken one.
-			// A valid configuration will be added to the total.
+// validate checks if the given variation is valid given the remaining groups of broken springs.
+func validate(s string, groups []int) (bool, bool) {
+	questions := strings.Count(s, "?")
+	brokens := strings.Count(s, "#")
 
-			springs[i] = '.'
-			count := countVariations(springs, groups, i+1)
+	// We've exhausted the groups but there are still more brokens.
+	// We've reached the end but it's invalid.
+	if brokens > 0 && len(groups) == 0 {
+		return false, true
+	}
 
-			springs[i] = '#'
-			count += countVariations(springs, groups, i+1)
+	// Replace all questions with operational springs.
+	s = strings.ReplaceAll(s, "?", ".")
 
-			springs[i] = '?'
-			return count
+	// We've reached the end if there are no more questions.
+	end := questions == 0
+
+	// Get the broken groups.
+	brokenGroups := strings.FieldsFunc(s, func(r rune) bool {
+		return r == '.'
+	})
+
+	// If the number of broken groups doesn't match the group count, it's invalid.
+	if len(brokenGroups) != len(groups) {
+		return false, end
+	}
+
+	// Check that the broken count matches the group.
+	for i, c := range groups {
+		if c != len(brokenGroups[i]) {
+			return false, end
 		}
 	}
 
-	// Found valid variation: count towards total.
-	if isValidVariation(springs, groups) {
-		return 1
-	}
-
-	// Not a valid variation: don't count towards total.
-	return 0
+	// All broken groups match group. This sequence is valid.
+	return true, true
 }
 
-func isValidVariation(springs []byte, groups []int) bool {
-	// fmt.Printf("springs: %v\n", string(springs))
-	group := 0
+// eat consumes the string for the given amount of brokens and returns the index.
+func eat(s string, group int) (int, bool) {
+	// There are less springs than there are broken ones for the group.
+	// No need to check.
+	if len(s) < group {
+		return 0, false
+	}
 
-	for i := 0; i < len(springs); i++ {
-		if springs[i] == '#' {
-			// Found broken spring; count the number of broke  springs and compare to current group.
-			start := i
-			for i < len(springs) && springs[i] == '#' {
-				i++
-			}
-
-			// Number of broken springs doesn't match the current group.
-			// This is an invalid variation.
-			if group < len(groups) && i-start != groups[group] {
-				return false
-			}
-
-			// Number of broken springs matches the number for the current group.
-			// Advance to the next group.
-			group++
+	// If there are any operational springs between the beginning and
+	// the size of the group, it's invalid.
+	for i := 0; i < group; i++ {
+		if s[i] == '.' {
+			return 0, false
 		}
 	}
 
-	// The variation is valid if all the groups have been verified.
-	return group == len(groups)
+	// Remaining number of springs exactly matches the number of broken ones in the group.
+	if len(s) == group {
+		return group, true
+	}
+
+	// The end of the group is a delimeter.
+	if s[group] == '.' || s[group] == '?' {
+		return group + 1, true
+	}
+
+	// Couldn't consume any springs.
+	return 0, false
+}
+
+func countArrangements(s string, groups []int, visited map[string]int) int {
+	key := s + fmt.Sprintf("%v", groups)
+	// fmt.Printf("s: %v\n", s)
+	if n, ok := visited[key]; ok {
+		return n
+	}
+
+	visited[key] = 0
+	if valid, end := validate(s, groups); end {
+		if valid {
+			// Found a valid variation.
+			visited[key] = 1
+			return 1
+		}
+
+		// Found invalid variation: don't count towards total.
+		return 0
+	}
+
+	if s[0] == '.' {
+		n := countArrangements(s[1:], groups, visited)
+		visited[key] = n
+		return n
+	}
+
+	n := 0
+	if s[0] == '?' {
+		n += countArrangements(s[1:], groups, visited)
+	}
+
+	cnt, ok := eat(s, groups[0])
+	if !ok {
+		// Couldn't consume anymore: we're done here.
+		visited[key] = n
+		return n
+	}
+
+	// Check the permutations for the rest of the string.
+	n += countArrangements(s[cnt:], groups[1:], visited)
+	visited[key] = n
+	return n
 }
