186d87013414a4230726fd208a496b4e060f0cd6
Kohei Hisakuni <khisakuni@gmail.com>
2025-09-20 12:50:09 -0700

    2024 day 9 part 2

--- 2024/08/main.go
+++ 2024/08/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"strings"
+	"time"
 )
 
 //go:embed input.txt
@@ -36,6 +37,102 @@ var input string
 func main() {
 	part1()
 
+	n := time.Now()
+	part2()
+	fmt.Printf("%s\n", time.Since(n))
+}
+
+func part2() {
+
+	lines := strings.Split(input, "\n")
+	grid := [][]rune{}
+	for _, line := range lines {
+		grid = append(grid, []rune(line))
+	}
+
+	freqToAntennas := map[rune][][]int{}
+	for y, line := range lines {
+		for x, c := range line {
+			if c == '.' {
+				continue
+			}
+
+			freqToAntennas[c] = append(freqToAntennas[c], []int{x, y})
+		}
+	}
+
+	m := map[string]bool{}
+
+	for _, antennas := range freqToAntennas {
+		for _, coords := range antennas {
+			for _, other := range antennas {
+				x, y := coords[0], coords[1]
+				xo, yo := other[0], other[1]
+
+				if x == xo && y == yo {
+					continue
+				}
+
+				xDist := x - xo
+				if xDist < 0 {
+					xDist *= -1
+				}
+
+				yDist := y - yo
+				if yDist < 0 {
+					yDist *= -1
+				}
+
+				var ax, axo int
+				if x < xo {
+					ax = -1 * xDist
+					axo = xDist
+				} else {
+					ax = xDist
+					axo = -1 * xDist
+				}
+
+				var ay, ayo int
+				if y < yo {
+					ay = -1 * yDist
+					ayo = yDist
+				} else {
+					ay = yDist
+					ayo = -1 * yDist
+				}
+
+				for x >= 0 && x < len(lines[0]) && y >= 0 && y < len(lines) {
+					m[fmt.Sprintf("%d-%d", x, y)] = true
+					grid[y][x] = '#'
+
+					y = y + ay
+					x = x + ax
+				}
+
+				for xo >= 0 && xo < len(lines[0]) && yo >= 0 && yo < len(lines) {
+					m[fmt.Sprintf("%d-%d", xo, yo)] = true
+					grid[yo][xo] = '#'
+
+					xo = xo + axo
+					yo = yo + ayo
+				}
+
+				// for _, l := range grid {
+				// 	fmt.Printf("%s\n", string(l))
+				// }
+
+				// fmt.Println("")
+			}
+
+		}
+	}
+
+	// for _, l := range grid {
+	// 	fmt.Printf("%s\n", string(l))
+	// }
+	// fmt.Println("")
+
+	fmt.Printf("part 2: %v\n", len(m))
 }
 
 func part1() {
@@ -121,5 +218,5 @@ func part1() {
 	// }
 	// fmt.Println("")
 
-	fmt.Printf("part: %v\n", len(m))
+	fmt.Printf("part 1: %v\n", len(m))
 }
