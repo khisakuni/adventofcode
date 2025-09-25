87f28157ee82a66850301d1c47efaac06b3deb3e
Kohei Hisakuni <khisakuni@gmail.com>
2025-09-25 13:31:35 -0700

    2024 day 9 part 2

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -13,7 +13,7 @@ var input string
 
 func main() {
 	part1()
-
+	part2()
 }
 
 func part1() {
@@ -56,4 +56,81 @@ func part1() {
 	}
 
 	fmt.Printf("part1: %v\n", checksum)
+}
+
+func part2() {
+	seq := []int{}
+	var fileID int
+	for i, c := range input {
+		num := int(c - '0')
+		isFile := (i % 2) == 0
+
+		if isFile {
+			for i := 0; i < num; i++ {
+				seq = append(seq, fileID)
+			}
+
+			fileID++
+		} else {
+			for i := 0; i < num; i++ {
+				seq = append(seq, -1)
+			}
+		}
+	}
+
+	right := len(seq) - 1
+	for right > 0 {
+		for seq[right] == -1 {
+			right--
+		}
+
+		end := right
+		start := end
+		current := seq[start]
+		for start >= 0 && seq[start] == current {
+			start--
+		}
+
+		l := end - start
+
+		emptyStart := 0
+		emptyEnd := 0
+		for emptyEnd < end {
+			for emptyStart < end && seq[emptyStart] > -1 {
+				emptyStart++
+			}
+
+			emptyEnd = emptyStart
+			for emptyEnd < end && seq[emptyEnd] == -1 {
+				emptyEnd++
+			}
+
+			if emptyEnd-emptyStart >= l {
+				for i := emptyStart; i < emptyStart+l; i++ {
+					seq[i] = current
+				}
+
+				for i := start + 1; i <= end; i++ {
+					seq[i] = -1
+				}
+				break
+			}
+
+			emptyStart = emptyEnd + 1
+		}
+
+		right = start
+	}
+
+	var checksum int
+	for i, c := range seq {
+		if c == -1 {
+			continue
+		}
+
+		checksum += i * c
+	}
+
+	fmt.Printf("part 2: %v\n", checksum)
+
 }
