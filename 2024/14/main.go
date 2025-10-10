a417abdf5671a44341994d4a444368a00f1fb3cf
Kohei Hisakuni <khisakuni@gmail.com>
2025-10-09 22:43:20 -0700

    2024 day 14 part 2

--- 2024/14/main.go
+++ 2024/14/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"strings"
+	// "time"
 )
 
 // var input = `p=0,4 v=3,-3
@@ -27,6 +28,7 @@ const height = 103
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -58,5 +60,68 @@ func part1() {
 	}
 
 	fmt.Printf("part 1: %v\n", topLeft*topRight*bottomLeft*bottomRight)
+}
 
+func part2() {
+	lines := strings.Split(input, "\n")
+	var grid [][]bool
+	for sec := 0; sec < 10000; sec++ {
+		grid = [][]bool{}
+		for i := 0; i < height; i++ {
+			grid = append(grid, make([]bool, width))
+		}
+		for _, line := range lines {
+			var x, y, vx, vy int
+			fmt.Sscanf(line, "p=%d,%d v=%d,%d", &x, &y, &vx, &vy)
+
+			x = ((vx*sec + x) % width)
+			if x < 0 {
+				x = width + x
+			}
+			y = ((vy*sec + y) % height)
+			if y < 0 {
+				y = height + y
+			}
+			grid[y][x] = true
+		}
+
+		area := 10
+		for i := 0; i < height-area; i += area {
+			for j := 0; j < width-area; j += area {
+
+				var pop int
+				for y := i; y < i+area; y++ {
+					for x := j; x < j+area; x++ {
+						if grid[y][x] {
+							pop++
+						}
+					}
+				}
+
+				if float32(pop)/float32((area*area)) > 0.5 {
+					fmt.Printf("part 2: %v\n", sec)
+					print(grid)
+
+					return
+				}
+			}
+
+		}
+	}
+
+}
+
+func print(grid [][]bool) {
+	for _, line := range grid {
+		for _, c := range line {
+			if c {
+				fmt.Printf("x")
+			} else {
+				fmt.Printf(".")
+			}
+		}
+		fmt.Println()
+	}
+
+	fmt.Println()
 }
